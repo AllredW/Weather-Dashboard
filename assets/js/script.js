a77e30b69dfbc8b9509cda60569de6e389e7359c
AllredW <allrewil@gmail.com>
2024-07-08 21:55:40 -0400

    Some html structure, linking APIs in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
 // Server-side API: Geo-coding API, converts cities in search input to latitude/longitude coordinates
-const geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
+const geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={449e0f70c68d023360a6656f43c00e19}';
 // Server-side API: requesting data from OpenWeather
-const openWeather = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+const openWeather = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={449e0f70c68d023360a6656f43c00e19}';
