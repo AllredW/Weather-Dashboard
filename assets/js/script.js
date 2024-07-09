b53c62462b733a00f080151c26e85502d642b46a
AllredW <allrewil@gmail.com>
2024-07-08 20:56:09 -0400

    Inital setup

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// Server-side API: Geo-coding API, converts cities in search input to latitude/longitude coordinates
+const geoCode = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
+// Server-side API: requesting data from OpenWeather
+const openWeather = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
