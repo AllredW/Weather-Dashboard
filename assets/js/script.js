7d77c2442979550a78cc85bed3f0e1a07576eb83
AllredW <allrewil@gmail.com>
2024-07-10 10:43:18 -0400

    adjustments to js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 const searchButton = document.getElementById("search-button");
 // utilizes Geocoding API to reference cities matching the text in the search input, including latitude and longitude
+
+// check localStorage for stored city names, or creates new array to store new data
+const searchData = localStorage.getItem("searchResults") || [];
+
 function fetchCity(data) {
     const city = document.getElementById("form1").value;
     const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=449e0f70c68d023360a6656f43c00e19`;
@@ -18,6 +22,7 @@ function fetchCity(data) {
     .catch(function (err) {
       console.error(err);
     });
+    storeData(city);
 };
 // extracts the latitude/longitude properties from the "city" data above, and uses the openWeather API to check weather conditions in the target city
 function fetchWeather(searchedCity) {
@@ -43,33 +48,19 @@ fetch(weatherUrl)
     .catch(function (err) {
       console.error(err);
     });
-    storeData(data);
+
 };
 
-// storage array
-const searchData = [];
 // stingifies data and pushes to localStorage, adds search data to Recent Searches
-function storeData(data) {
-const searchData = localStore();
-
-searchData.push(data);
-
-const stringArray = JSON.stringify(searchData);
-localStorage.setItem('searchData', stringArray);
-console.log(searchData);
-};
-
-const localStore = function () {
-  const storeData = localStorage.getItem("searchData");
-  if (storeData) {
+// take city from search form, as string, 
+function storeData(searchCity) {
+  // check for data
+ console.log(searchCity);
+ console.log(searchData);
     // parse data from string
-    const parseArray = JSON.parse(storeData);
-
-    // display parseArray on page
-    return parseArray;
-  } else {
-    return [];
-  }
+    searchData.push(searchCity);
+  // stringify/push searchData array into localStorage
+localStorage.setItem("searchResults", JSON.stringify(searchData));
 };
 
 searchButton.addEventListener("click", fetchCity);
